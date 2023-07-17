a5b1d58b4af513bffbe078e8081803581fad7a67
student <student@gnu.ac.in>
2023-07-17 08:23:06 +0530

    practical 1

---
+++ src/main/kotlin/P1.1.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var a : Int = 22
+    val a1 : Int = 5
+    var b : Float = 1.5f
+    val b1 : Float = 1.2f
+    val c : String = "Het"
+    val c1 : String = "Patel"
+
+    println("Int value : "+a)
+    println("Int Constant value : "+a1)
+    println("Float value : "+b)
+    println("Float Constant value : "+b1)
+    println("String value : "+c)
+    println("String Constant value : "+c1)
+}
---
+++ src/main/kotlin/P1.2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var a : Int = 10
+    var b = a.toDouble()
+    var c : String = 10.toString()
+    var d = c.toInt()
+
+    println("Int value : "+a)
+    println("Double value : "+b)
+    println("String value : "+c)
+    println("Int value (from string) : "+a)
+
+}
